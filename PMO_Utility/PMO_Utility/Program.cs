09f2489a488cb369f6c5c7615cb101ee359f3c70
INDERPREET SINGH <inderpreetsinghvohra@gmail.com>
2020-04-01 00:34:49 +0530

    changed the commit statement

--- PMO_Utility/PMO_Utility/Program.cs
+++ PMO_Utility/PMO_Utility/Program.cs
@@ -6,7 +6,7 @@ namespace PMO_Utility
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the pmo project");
         }
     }
 }
